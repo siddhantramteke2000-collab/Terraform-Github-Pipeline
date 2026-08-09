d83aac0c077529cd936404360847cb941585b88a
Siddhant Ramteke <siddhantramteke2000@gmail.com>
2026-08-10 01:02:38 +0530

    RG and VNET

--- main.tf
+++ main.tf
@@ -18,6 +18,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-sud"
+  name     = "rg-sid"
   location = "centralindia"
+}
+resource "azurerm_virtual_network" "vnet" {
+  depends_on          = [azurerm_resource_group.rg]
+  name                = "sid-vnet"
+  location            = "centralindia"
+  resource_group_name = "rg-sid"
+  address_space       = ["10.0.0.0/16"]
 }
