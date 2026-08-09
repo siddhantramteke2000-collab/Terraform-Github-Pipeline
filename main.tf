93bb19ea4cc159ddd2137ea26467722de5908036
Siddhant Ramteke <siddhantramteke2000@gmail.com>
2026-08-09 19:17:31 +0530

    Pipeline

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.78.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "dev-rg"
+    storage_account_name = "siddhantkastorage"
+    container            = "storetfstate"
+    key                  = "practice.tfstate"
+  }
 }
 provider "azurerm" {
   features {}
